badbf5ec1b263396e6b1a45477d7b798c71cc62e
jesusamoros <37039972+jesusamoros@users.noreply.github.com>
2019-12-10 15:24:57 +0100

    Create Backup_files_old.sh

---
+++ Backup_files_old.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+d=$(date +'%e_%b_%Y')
+echo "Creando directorio"
+mkdir $d.backup
+
+echo "Buscando archivos con antiguedad de mas de 10 dias y comprimiendo"
+find . -name '*.log' -type f -mtime +10 -exec gzip -9 {} \;find . -name '*.log' -type f -mtime +10 -exec gzip -9 {} \;
+
+echo "Moviendo"
+mv  *.gz  $d.backup/
+
+echo "Comprimiendo el directorio de backups"
+tar -czvf $d.logs.tar.gz $d.backup/
+
